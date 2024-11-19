7e3eed570af8cd5e9882ad6521d7d27b99c4fc66
Jeff Crowder <jeff.crowder@gmail.com>
2024-11-19 15:44:47 -0500

    Initial Commit

---
+++ e2e/popup.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from "./fixtures";
+import { openPopup } from "./pages/popup";
+
+test("Popup counter increments when clicked", async ({ page, extensionId }) => {
+  const popup = await openPopup(page, extensionId);
+  expect(await popup.getCounterText()).toEqual("count is 0");
+
+  await popup.clickCounter();
+  expect(await popup.getCounterText()).toEqual("count is 1");
+
+  await popup.clickCounter();
+  expect(await popup.getCounterText()).toEqual("count is 2");
+
+});
+
+
+test("Popup counter increments when clicked (fake error)", async ({ page, extensionId }) => {
+  const popup = await openPopup(page, extensionId);
+  expect(await popup.getCounterText()).toEqual("count is 0");
+
+  await popup.clickCounter();
+  expect(await popup.getCounterText()).toEqual("count is 1");
+
+  await popup.clickCounter();
+  expect(await popup.getCounterText()).toEqual("count is 2");
+
+  await popup.clickCounter();
+  expect(await popup.getCounterText()).toEqual("count is 2");
+
+});
